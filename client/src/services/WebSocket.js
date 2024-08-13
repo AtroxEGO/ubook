a3f6f9b17a22802b5802bee83ccd38e1b564f063
Paweł Polakiewicz <pawel.polakiewicz1@gmail.com>
2024-08-13 17:30:15 +0200

    websocket change

--- client/src/services/WebSocket.js
+++ client/src/services/WebSocket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.REACT_APP_API_HOST);
+export const socket = io(process.env.REACT_APP_API_HOST, {
+  transports: ["websocket"],
+  path: "/socket.io",
+  autoConnect: false,
+});
 
 export const closeWebSocketConnection = () => {
   socket.disconnect();
